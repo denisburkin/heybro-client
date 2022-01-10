4d8ccc050a660bd56f6b3c0b779cdef8730123d2
Denis Burkin <calipsio.com@gmail.com>
2022-01-10 23:54:23 +0300

    update

--- src/routes.php
+++ src/routes.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/dashboard', function () {
-
-    return view('heybro-client::main');
+    return view('heybro-client::heybro-client.main');
 });
